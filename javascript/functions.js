b1140ce60fb79bdbdfa542849290514fa545689c
Tom <40807906+tlan2@users.noreply.github.com>
2020-08-02 19:36:05 -0700

    Search produces HITTING or PITCHING stats depending on player position.

--- javascript/functions.js
+++ javascript/functions.js
@@ -26,8 +26,9 @@ function fetchAllPlayerInfo(name){
       return response.json();
     })
     .then(function(data){
-        console.log("data", data);
-        console.log("Active totalSize = " + data.search_player_all.queryResults.totalSize)
+        console.log("activePlayerInfo", data);
+        var totalSize = data.search_player_all.queryResults.totalSize;
+        console.log("Active totalSize = " + data.search_player_all.queryResults.totalSize);
 
         if(data.search_player_all.queryResults.totalSize == 0){
               console.log("Not active.")
@@ -37,7 +38,7 @@ function fetchAllPlayerInfo(name){
                   const name = data.search_player_all.queryResults.row.name_display_first_last;
                   const position = data.search_player_all.queryResults.row.position;
           
-                  const player = `<h1 id="name">${name} - ${position}</h1>`;
+                  const player = `<h1 id="active">${name} - ${position}</h1>`;
 
                   console.log(name);
                   console.log(position);
@@ -73,28 +74,45 @@ function fetchRetiredPlayerInfo(urlName){
         console.log("retiredData", retiredData);
         var totalSize = retiredData.search_player_all.queryResults.totalSize;
         console.log("Retired totalSize = " + totalSize);
-        console.log(typeof totalSize)
       
         if(totalSize == "0"){
           console.log("in totalSize condition");
           document.getElementById('nameResult').innerHTML = playerDoesNotExist();
           return;
+
+        } else if(totalSize > 1){
+          console.log("More than 1 player with same name.");
+
+          const id = retiredData.search_player_all.queryResults.row['0'].player_id;
+          const name = retiredData.search_player_all.queryResults.row['0'].name_display_first_last;
+          const position = retiredData.search_player_all.queryResults.row['0'].position;
+
+          console.log("first id of 2 players = " + id);
+          console.log("Retired name" + name);
+          console.log("Retired positon" + position);
+
+          const player = `<h1 id="retired">${name} - ${position}</h1>`;
+          document.getElementById('nameResult').innerHTML = player;
+          fetchPlayerCareerStats(id, position);
+
+        } else {
+          console.log("Retired Player stats retrieval.");
+
+          const id = retiredData.search_player_all.queryResults.row.player_id;
+          const name = retiredData.search_player_all.queryResults.row.name_display_first_last;
+          const position = retiredData.search_player_all.queryResults.row.position;
+  
+          console.log("retired id = " + id);
+          console.log("Retired name" + name);
+          console.log("Retired position " + position);
+
+          const player = `<h1 id="retired">${name} - ${position}</h1>`;
+          document.getElementById('nameResult').innerHTML = player;
+    
+          fetchPlayerCareerStats(id, position);
         }
 
-        console.log("Retired Player stats retrieval.");
-        const id = retiredData.search_player_all.queryResults.row.player_id;
-        console.log("id = " + id);
-        const name = retiredData.search_player_all.queryResults.row.name_display_first_last;
-        const position = retiredData.search_player_all.queryResults.row.position;
-  
-        const player = `<h1 id="name">${name} - ${position}</h1>`;
-
-        console.log(name);
-        console.log(position);
-  
-        document.getElementById('nameResult').innerHTML = player;
-  
-        fetchPlayerCareerStats(id);
+        
             
     })
     .catch(err => {
@@ -102,9 +120,13 @@ function fetchRetiredPlayerInfo(urlName){
     });
   }
 
-function fetchPlayerCareerStats(id){
+function fetchPlayerCareerStats(id, position){
 // Fetches player's CAREER REGULAR SEASON STATS
-fetch(`https://mlb-data.p.rapidapi.com/json/named.sport_career_hitting.bam?player_id='${id}'&game_type='R'&league_list_id='mlb'`, {
+console.log("position = " + position);
+
+if(position == "P"){
+  console.log("PITCHER career stats");
+  fetch(`https://mlb-data.p.rapidapi.com/json/named.sport_career_pitching.bam?player_id='${id}'&league_list_id='mlb'&game_type='R'`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "mlb-data.p.rapidapi.com",
@@ -115,49 +137,64 @@ fetch(`https://mlb-data.p.rapidapi.com/json/named.sport_career_hitting.bam?playe
     return response.json();
   })
   .then(function(careerData){
-          console.log("careerData", careerData)
+          console.log("PITCHER careerData", careerData)
 
-          const hr = careerData.sport_career_hitting.queryResults.row.hr;
-          const avg = careerData.sport_career_hitting.queryResults.row.avg;
-          const rbi = careerData.sport_career_hitting.queryResults.row.rbi;
-          const slg = careerData.sport_career_hitting.queryResults.row.slg;
-          const obp = careerData.sport_career_hitting.queryResults.row.obp;
-          const r = careerData.sport_career_hitting.queryResults.row.r;
-          const sb = careerData.sport_career_hitting.queryResults.row.sb;
-          const so = careerData.sport_career_hitting.queryResults.row.so;
-          const bb = careerData.sport_career_hitting.queryResults.row.bb;
+          const games = careerData.sport_career_pitching.queryResults.row.g;
+          const wins = careerData.sport_career_pitching.queryResults.row.w;
+          const losses = careerData.sport_career_pitching.queryResults.row.l;
+          const winPct = careerData.sport_career_pitching.queryResults.row.wpct;
+          const era = careerData.sport_career_pitching.queryResults.row.era;
+          const gamesStarted = careerData.sport_career_pitching.queryResults.row.gs;
+          const completeGames = careerData.sport_career_pitching.queryResults.row.cg;
+          const shutouts = careerData.sport_career_pitching.queryResults.row.sho;
+          const onBasePlusSlugging = careerData.sport_career_pitching.queryResults.row.ops;
+          const battingAvg = careerData.sport_career_pitching.queryResults.row.avg;
+          const kPer9 = careerData.sport_career_pitching.queryResults.row.k9;
+          const strikeOuts = careerData.sport_career_pitching.queryResults.row.so;
+          const innningsPitched = careerData.sport_career_pitching.queryResults.row.ip;
+          const runsPer9 = careerData.sport_career_pitching.queryResults.row.rs9;
 
           const html = `<div class="row">
-                          <div class="col-sm-4" id="homeRuns">
-                            Home Runs - ${hr}
+                          <div class="col-sm-4" id="games">
+                            Games - ${games}
                           </div>
-                          <div class="col-sm-4" id="battingAvg">
-                            Batting Avg. - ${avg}
+                          <div class="col-sm-4" id="wpct">
+                            Wins-Losses-Win Pct - ${wins}-${losses}-${winPct}
                           </div>
-                          <div class="col-sm-4" id="rbi">
-                            Runs Batted In (RBI) - ${rbi}
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-sm-4" id="slg">
-                            Slugging Pct (SLG) - ${slg}
-                          </div>
-                          <div class="col-sm-4" id="obp">
-                            On Base Pct (OBP) - ${obp}
-                          </div>
-                          <div class="col-sm-4" id="runs">
-                            Runs (R) - ${r}
+                          <div class="col-sm-4" id="era">
+                            Earned Run Average (ERA) - ${era}
                           </div>
                         </div>
                         <div class="row">
-                          <div class="col-sm-4" id="steals">
-                            Steals (SB) - ${sb}
+                          <div class="col-sm-4" id="ops">
+                            On-base Slugging Pct - ${onBasePlusSlugging}
+                          </div>
+                          <div class="col-sm-4" id="avg">
+                            Opponent Batting Avg. (AVG) - ${battingAvg}
+                          </div>
+                          <div class="col-sm-4" id="runsPer9">
+                            Runs Per 9-Innings (rs9) - ${runsPer9}
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-sm-4" id="games">
+                            Games Started (GS) - ${gamesStarted}
+                          </div>
+                          <div class="col-sm-4" id="completeGames">
+                            Complete Games (CG) - ${completeGames}
+                          </div>
+                          <div class="col-sm-4" id="shutOuts">
+                            Shut Outs (SHO) - ${shutouts}
+                          </div>
+                          <div class="row">
+                          <div class="col-sm-4" id="inningsPitched">
+                            Innings Pitched (IP) - ${innningsPitched}
                           </div>
                           <div class="col-sm-4" id="strikeOuts">
-                            Strike Outs (SO) - ${so}
+                            Strike Outs (SO) - ${strikeOuts}
                           </div>
-                          <div class="col-sm-4" id="walks">
-                            Walks (BB) - ${bb}
+                          <div class="col-sm-4" id="kPer9">
+                            Strike Outs Per 9-Innings (k9) - ${kPer9}
                           </div>
                         </div>`
           
@@ -166,6 +203,73 @@ fetch(`https://mlb-data.p.rapidapi.com/json/named.sport_career_hitting.bam?playe
   .catch(err => {
     console.log(err);
   });
+} else {
+    console.log("HITTER career stats");
+    fetch(`https://mlb-data.p.rapidapi.com/json/named.sport_career_hitting.bam?player_id='${id}'&game_type='R'&league_list_id='mlb'`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "mlb-data.p.rapidapi.com",
+      "x-rapidapi-key": "99277645d6msh6f62f53f4354a9fp1bbe68jsnfe46c1d9dfb5"
+    }
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(function(careerData){
+            console.log("HITTER careerData", careerData)
+
+            const hr = careerData.sport_career_hitting.queryResults.row.hr;
+            const avg = careerData.sport_career_hitting.queryResults.row.avg;
+            const rbi = careerData.sport_career_hitting.queryResults.row.rbi;
+            const slg = careerData.sport_career_hitting.queryResults.row.slg;
+            const obp = careerData.sport_career_hitting.queryResults.row.obp;
+            const r = careerData.sport_career_hitting.queryResults.row.r;
+            const sb = careerData.sport_career_hitting.queryResults.row.sb;
+            const so = careerData.sport_career_hitting.queryResults.row.so;
+            const bb = careerData.sport_career_hitting.queryResults.row.bb;
+
+            const html = `<div class="row">
+                            <div class="col-sm-4" id="homeRuns">
+                              Home Runs - ${hr}
+                            </div>
+                            <div class="col-sm-4" id="battingAvg">
+                              Batting Avg. - ${avg}
+                            </div>
+                            <div class="col-sm-4" id="rbi">
+                              Runs Batted In (RBI) - ${rbi}
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-sm-4" id="slg">
+                              Slugging Pct (SLG) - ${slg}
+                            </div>
+                            <div class="col-sm-4" id="obp">
+                              On Base Pct (OBP) - ${obp}
+                            </div>
+                            <div class="col-sm-4" id="runs">
+                              Runs (R) - ${r}
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col-sm-4" id="steals">
+                              Steals (SB) - ${sb}
+                            </div>
+                            <div class="col-sm-4" id="strikeOuts">
+                              Strike Outs (SO) - ${so}
+                            </div>
+                            <div class="col-sm-4" id="walks">
+                              Walks (BB) - ${bb}
+                            </div>
+                          </div>`
+            
+          document.getElementById('stats').innerHTML = html;              
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+
 }
 
 //============================================================================
@@ -201,20 +305,20 @@ function playerDoesNotExist(){
   var subheader;
   var j = i-1
   if(j == 0){
-    subheader = "<h5 id=\"name\">This is awkward....</h5>"
+    subheader = "<h5 id=\"missing\">This is awkward....</h5>"
   } else if(j == 1){
-    subheader = "<h5 id=\"name\">Maybe they've gone missing.</h5>"
+    subheader = "<h5 id=\"missing\">Maybe they've gone missing.</h5>"
   } else if(j == 2){
-    subheader = "<h5 id=\"name\">There are plenty of wannabes out there though.</h5>"
+    subheader = "<h5 id=\"missing\">There are plenty of wannabes out there though.</h5>"
   } else if(j == 3){
-    subheader = "<h5 id=\"name\">Sorry to break it to you.</h5>"
+    subheader = "<h5 id=\"missing\">Sorry to break it to you.</h5>"
   }
   var html = `<br>
-              <h3 id="name">Player Does Not Exist.</h3>
+              <h3 id="missing">Player Does Not Exist.</h3>
               <br>
               ${subheader}
               <br>
-              <img src="images/missing/${missing[i-1]}" id="missing" alt="Player Does Not Exist">`;
+              <img src="images/missing/${missing[i-1]}" id="missingPic" alt="Player Does Not Exist">`;
 
   return html;
 }
@@ -224,7 +328,7 @@ function playerDoesNotExist(){
 //============================================================================
 
 function fetchPlayerByID(id){
-// Provides more PERSONAL info the player including twitter handle
+// Provides ALL PERSONAL info the player including twitter handle
 
     fetch(`https://mlb-data.p.rapidapi.com/json/named.player_info.bam?sport_code='mlb'&player_id='${id}'`, {
       "method": "GET",
